cea60ecfe53ff79216ec38925ea2af001c2312a5
zengpuyang <zengpuyang>
2015-02-02 19:22:39 +0800

    radio and check adjust

---
+++ calculate.js
@@ -0,0 +1,16 @@
+function radio_calculate(name,value) {
+    document.getElementById(name).innerText=value
+}
+function checkbox_calculate(name) {
+    var value='';
+    for(var list_number= 0;list_number<4;list_number++){
+        var list=document.getElementsByName(name);
+        if(list[list_number].checked){
+            value+=list[list_number].value
+        }
+    }
+    document.getElementById(name).innerText=value
+}
+function calculate() {
+
+}
