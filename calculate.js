4585994fc88cc3bddb8d61570c7cc969b695f186
zengpuyang <zengpuyang>
2015-02-03 21:17:20 +0800

    mind change and add the question_type function

--- calculate.js
+++ calculate.js
@@ -1,16 +1,25 @@
-function radio_calculate(name,value) {
-    document.getElementById(name).innerText=value
-}
-function checkbox_calculate(name) {
+function get_radio_answer_calculate(name) {
     var value='';
-    for(var list_number= 0;list_number<4;list_number++){
-        var list=document.getElementsByName(name);
-        if(list[list_number].checked){
-            value+=list[list_number].value
+    for(var answer_list= 0;answer_list<4;answer_list++){
+        var answers=document.getElementsByName(name);
+        if(answers[answer_list].checked){
+            value+=answers[answer_list].value
         }
     }
-    document.getElementById(name).innerText=value
+    return value
+}
+function save_answer_calculate(name,value) {
+
 }
 function judge_calculate() {
+    var model_answer=answers(),total_score= 0;
+    for(var qid= 0;qid<model_answer.length;qid++){
+    }
 
+
+
+
+
+    document.getElementsByName('class')[0].focus();
+    document.getElementById('score').innerText='分'
 }
