a9a5df312c643608cac080f958de04f2c54d2b20
zengpuyang <zengpuyang>
2015-02-04 21:38:45 +0800

    judging adjust

--- calculate.js
+++ calculate.js
@@ -9,8 +9,6 @@ function gap_answer_calculate() {
         var answer=document.getElementById(gap.qid).value;
         gap_filling_judge_calculate(gap,answer);
         scores[scores.length]=[gap.qid,gap.score];
-
-        console.log(gap.score)
     }
     return scores
 }
@@ -23,11 +21,10 @@ function gap_filling_judge_calculate(gap_filling,my_answer) {
     }
     return gap_filling.score
 }
-/*function get_radio_answer_calculate(name) {
-    var value='';
-    for(var answer_list= 0;answer_list<4;answer_list++){
-        var answers=document.getElementsByName(name),option=(answers[answer_list]).checked;
-        if(option){
+function get_choice_answer_calculate(name) {
+    var value='',answers=document.getElementsByName(name);
+    for(var answer_list= 0;answer_list<answers.length;answer_list++){
+        if((answers[answer_list]).checked){
             value+=answers[answer_list].value
         }
     }
@@ -38,17 +35,17 @@ function choice_calculate() {
     var gap_question=question_types().choice;
     for(var number=0;number<gap_question.length;number++){
         var model_answer=gap_question[number][1],qid=gap_question[number][0];
-        var answer=get_radio_answer_calculate(qid),score=gap_question[number][2];
+        var answer=get_choice_answer_calculate(qid),score=gap_question[number][2];
         if(model_answer==answer){
             scores[scores.length]=[qid,score]
         }
         console.log(answer)
     }
     return scores
-}*/
+}
 function calculate() {
     gap_answer_calculate();
-
+    choice_calculate();
 
 
     document.getElementsByName('class')[0].focus();
