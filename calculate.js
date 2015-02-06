3128bace3935418b98e4d4819452ec3d094346ba
zengpuyang <zengpuyang>
2015-02-06 18:33:13 +0800

    special gap filling edited

--- calculate.js
+++ calculate.js
@@ -1,4 +1,22 @@
-function gap_answer_calculate() {
+function special_gap_filling_answer_adjust_calculate(qid) {
+    var answers=document.getElementsByName(qid),answer='';
+    for (var qid_list=0;qid_list<answers.length;qid_list++){
+        answer+=answers[qid_list].value
+    }
+    return answer
+}
+function gap_filling_judge_calculate(gap_filling,my_answer) {
+    for (var answer_number=0;answer_number<gap_filling.model_answer.length;answer_number++){
+        var model_answer_array=gap_filling.model_answer[answer_number];
+        if(my_answer.indexOf(model_answer_array)!=-1){
+            gap_filling.score+=gap_filling.sub_score
+
+
+        }
+    }
+    return gap_filling.score
+}
+function gap_answer_adjust_calculate() {
     var scores=[],gap={};
     gap.answers=question_types().gap_filling;
     for(var number=0;number<gap.answers.length;number++){
@@ -6,20 +24,11 @@ function gap_answer_calculate() {
         gap.qid=gap.answers[number][0];
         gap.sub_score=gap.answers[number][2];
         gap.score=0;
-        var answer=document.getElementById(gap.qid).value;
+        var answer=special_gap_filling_answer_adjust_calculate(gap.qid);
         gap_filling_judge_calculate(gap,answer);
         scores[scores.length]=[gap.qid,gap.score];
     }
     return scores
-}
-function gap_filling_judge_calculate(gap_filling,my_answer) {
-    for (var answer_number=0;answer_number<gap_filling.model_answer.length;answer_number++){
-        var model_answer_array=gap_filling.model_answer[answer_number];
-        if(my_answer.indexOf(model_answer_array)!=-1){
-            gap_filling.score+=gap_filling.sub_score
-        }
-    }
-    return gap_filling.score
 }
 function get_choice_answer_calculate(name) {
     var value='',answers=document.getElementsByName(name);
@@ -30,7 +39,7 @@ function get_choice_answer_calculate(name) {
     }
     return value
 }
-function choice_calculate() {
+function choice_adjust_calculate() {
     var scores=[];
     var gap_question=question_types().choice;
     for(var number=0;number<gap_question.length;number++){
@@ -49,11 +58,25 @@ function sub_score_calculate(score_array) {
     }
     return sub
 }
+function customer() {
+    var customer_required=document.getElementsByName('required'),judge=true;
+    for(var customer_number= 0;customer_number<customer_required.length;customer_number++){
+    if(customer_required[customer_number].value==''){
+        judge=false;
+    }
+    }
+    return judge
+}
 function calculate() {
-    var gap_scores=sub_score_calculate(gap_answer_calculate());
-    var choice_scores=sub_score_calculate(choice_calculate());
-    var total=gap_scores+choice_scores;
-    document.getElementsByName('score')[0].disabled=true;
-    document.getElementsByName('class')[0].focus();
-    document.getElementById('score').innerText=total+'分'
+    var gap_scores=sub_score_calculate(gap_answer_adjust_calculate());
+    var choice_scores=sub_score_calculate(choice_adjust_calculate());
+    var total=gap_scores+choice_scores,custom=customer();
+    document.getElementsByName('required')[0].focus();
+    if(custom){
+        document.getElementsByName('score')[0].disabled=true;
+        document.getElementById('score').innerText=total+'分'
+    }
+    else{
+        alert('请确定班级、学号、姓名')
+    }
 }
